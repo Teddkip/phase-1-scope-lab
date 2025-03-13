2c2b7a59706af95687430449f10d93a53e478223
teddkibet6 <teddkibet6@gmail.com>
2025-03-13 03:29:02 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare a global variable customerName
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Declaring a global variable
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; // Changing the value of bestCustomer
+}
+
+// Declare a constant leastFavoriteCustomer
+const leastFavoriteCustomer = 'some value';
+
+// Function to change leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value'; // Attempting to change the constant
+}
